967a7b6237a904f8671aa5fb1c8efeb5ee0a5e4c
thion96 <thion96@mail.ru>
2024-12-16 22:23:57 +0300

    first commit

---
+++ payloadDll/dllmain.cpp
@@ -0,0 +1,22 @@
+﻿#include <Windows.h>
+#include <string>
+#include <iostream>
+
+void MsgBoxPayload() {
+    MessageBoxA(nullptr, "Hacking With ru-sfera.pw", "Wow!", MB_OK | MB_ICONINFORMATION);
+}
+
+BOOL APIENTRY DllMain(HMODULE hModule, DWORD dwReason, LPVOID lpReserved) {
+    switch (dwReason) {
+    case DLL_PROCESS_ATTACH:
+        MsgBoxPayload();
+        break;
+
+    case DLL_THREAD_ATTACH:
+    case DLL_THREAD_DETACH:
+    case DLL_PROCESS_DETACH:
+        break;
+    }
+
+    return TRUE;
+}
